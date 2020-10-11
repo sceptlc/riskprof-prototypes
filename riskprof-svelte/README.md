514a1daffa09e55280ae857d1a8409f406ca2276
sceptlc <sceptlc@bk.ru>
2020-10-11 10:40:42 +0300

    Svelte setup

---
+++ riskprof-svelte/README.md
@@ -0,0 +1,1 @@
+## Riskprof React prototype
---
+++ riskprof-svelte/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { Col, Container, Row } from 'sveltestrap';
+	
+	let name = "Bogdan";
+</script>
+
+<main>
+	<Container>
+		<Row>
+			<Col>
+				<h1>Hello {name}!</h1>
+			</Col>
+		</Row>
+
+	</Container>
+</main>
+
+<style>
+</style>
